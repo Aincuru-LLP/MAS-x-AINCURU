db77ac986df4c854a76ebfcf6e796bffe5349b36
Tamilselvan <tamilselvanjegan408@gmail.com>
2026-09-01 10:53:56 +0530

    backend

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -70,7 +70,10 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    const staticPath = path.resolve(__dirname, "..", "..", "dist", "public");
+    const staticPath =
+      process.env.NODE_ENV === "production"
+        ? path.resolve(__dirname, "public")
+        : path.resolve(__dirname, "..", "..", "dist", "public");
     app.use(express.static(staticPath));
     app.get("*", (_req, res) => {
       res.sendFile(path.join(staticPath, "index.html"));
